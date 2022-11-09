9a3c2d1c18066ea1fc9430b684598792845d4fe6
CistusF <sora6com@gmail.com>
2022-11-10 08:07:57 +0900

    add my tech and edit css

--- pages/index.js
+++ pages/index.js
@@ -37,13 +37,67 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.container}>
-        <main className={styles.main}>
-          <h1 className={styles.fadeIn}>Hi</h1>
+        <div className={styles.main}>
+          <h2 className={styles.fadeIn}>Who am i?</h2>
           <h1 className={styles.slideIn}>I{"'"}m CistusF</h1>
-        </main>
-        <main className={styles.main}>
-          <h1 className={styles.slideIn}>Animation Test</h1>
-        </main>
+        </div>
+        <div className={styles.main}>
+          <div className={styles.tech}>
+            <h1 className={`${styles.fadeIn} ${styles.techItem}`}>I can use</h1>
+            <div className={`${styles.slideIn} ${styles.techItem} ${styles.stacks}`}>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>C/C++</li>
+              <li>Python</li>
+              <ul>
+                <li>selenium</li>
+                <li>Flask</li>
+              </ul>
+              <li>Node.js</li>
+              <ul>
+                <li>Express</li>
+                <li>Discord.js</li>
+                <li>Typescript</li>
+                <li>React</li>
+                <li>Nextjs</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className={styles.main}>
+          <div className={styles.tech}>
+            <div className={`${styles.slideIn} ${styles.techItem} ${styles.stacks}`}>
+              <li>Database</li>
+              <ul>
+                <li>MongoDB</li>
+                <li>Mysql</li>
+              </ul>
+              <li>Front-End</li>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>React</li>
+                <li>Next.js</li>
+              </ul>
+              <li>Back-End</li>
+              <ul>
+                <li>Express</li>
+              </ul>
+              <li>ChatBot</li>
+              <ul>
+                <li>Discord</li>
+                <li>KakaoTalk</li>
+                <li>Telegram</li>
+                <li>Twitch</li>
+              </ul>
+              <li>App</li>
+              <ul>
+                <li>Electron</li>
+              </ul>
+            </div>
+          <h1 className={`${styles.fadeIn} ${styles.techItem}`}>Technology stack</h1>
+          </div>
+        </div>
       </div>
       <Footer />
     </>
