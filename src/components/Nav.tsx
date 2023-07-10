79a90a6fb260e227aec78218ccfa5cb9227331f2
Vendetta <ananddechannel@gmail.com>
2023-07-10 21:17:30 +0900

    Nav animation remove Nextjs 13.x issue

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,11 +7,13 @@ export default function Nav() {
         <div className={styles.nav}>
             <div className={styles.items}>
                 <Link href={"/#"} scroll={false}>
-                    <Image src={"/profile.jpeg"} alt="profile" width={50} height={50} />
+                    <Image src={"/profile.jpeg"} alt={"profile"} width={50} height={50} />
                 </Link>
-                <Link href={"/#about"} scroll={false}>About</Link>
-                <Link href={"/#projects"} scroll={false}>Projects</Link>
-                <Link href={"/#contact"} scroll={false}>Contact</Link>
+                <Link href={{ pathname: "/", hash: 'about' }} replace>
+                    <p>About</p>
+                </Link>
+                <Link href={{ pathname: "/", hash: 'projects' }} replace>Projects</Link>
+                <Link href={{ pathname: "/", hash: 'contact' }} replace>Contact</Link>
             </div>
             <div className={styles.divider} />
             <div className={styles.links}>
